1b3ec7eea42245044bb139a81d8e8460690291c6
ConorMcKillen <conormckillen@protonmail.com>
2021-09-15 21:48:07 +0100

    Initial commit

---
+++ src/Snake.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Conor McKillen on 15/09/2021
+ * UPDATE PROGRAM COMMENTS ABOUT PROGRAM HERE
+ **/
+public class Snake
+{
+
+   public static void main(String[] args)
+   {
+      new GameFrame();
+   } //main
+
+} //class
